d54cfee2b36920c24964ab9576dffdac109304de
Lakinana Chakradhar <140059923+chakradharlucky@users.noreply.github.com>
2023-09-29 16:35:35 +0530

    Update guessMyNumber.js

--- guessMyNumber.js
+++ guessMyNumber.js
@@ -1,5 +1,6 @@
 'use strict'
 let guessNumber = Math.trunc(Math.random()*20+1);
+let reset = false;
 // document.querySelector('.number').textContent = guessNumber;
 function displayMessage(msg) {
    document.querySelector(".message").textContent = msg ; 
@@ -9,15 +10,15 @@ let score = 20;
 let highscore = 0;
 
 document.querySelector(".check").addEventListener('click',function(){
+   if(!reset){
    let guessedNumber = Number(document.querySelector('.guess').value);
-   // document.querySelector('.number').textContent = guessNumber;
-   // console.log(guessedNumer)
    if(!guessedNumber){
       displayMessage("⛔ No number!");
       return;
    }
   if(guessNumber===guessedNumber)
   {
+     reset = true;
    document.querySelector('.number').textContent = guessedNumber;
    displayMessage("🎉Correct number! Click Again! button to play again...... ");
    if(highscore<score)
@@ -46,10 +47,10 @@ document.querySelector(".check").addEventListener('click',function(){
    else{
    displayMessage("📉 Number is too low");
    }
-
-}
+}}
 })
 document.querySelector(".again").addEventListener("click",function(){
+   reset = false;
    guessNumber = Math.trunc(Math.random()*20+1);
    document.querySelector('body').style = "backGround: #222" ;
    document.querySelector('.number').textContent ='?' ;
@@ -57,11 +58,4 @@ document.querySelector(".again").addEventListener("click",function(){
    document.querySelector('.score').textContent = 20 ;
    displayMessage("Start guessing.....");
    document.querySelector('.guess').value = " ";
-
-   
-
 })
-// let variable = document.querySelector(".number");
-// console.log(variable.textContent); 
-// variable.textContent=guessNubmer;
-// console.log(variable.textContent); 
